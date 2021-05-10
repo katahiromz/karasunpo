93348d00db26c8b85e40e7f61084cbeaa30095fe
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2021-05-10 11:18:09 +0900

    update tiff batch

--- lib/tiff-4.3.0-build.bat
+++ lib/tiff-4.3.0-build.bat
@@ -1,3 +1,3 @@
 del CMakeCache.txt
-cmake -DCMAKE_BUILD_TYPE=Release -G Ninja . -DBUILD_SHARED_LIBS=OFF
+cmake -DCMAKE_BUILD_TYPE=Release -G Ninja . -DBUILD_SHARED_LIBS=OFF -DZLIB_INCLUDE_DIR:PATH=C:/dev/KarasunpoProject/lib/zlib-1.2.11 -DZLIB_LIBRARY_RELEASE:FILEPATH=C:/dev/KarasunpoProject/lib/zlib-1.2.11/libz.a
 ninja
