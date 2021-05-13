10f26270e644d28f4b5c80ed33c12e8687d7397f
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2021-05-13 20:39:55 +0900

    version up to 1.0

--- lib/freeglut-3.2.1-build.bat
+++
@@ -1,3 +0,0 @@
-del CMakeCache.txt
-cmake -DCMAKE_BUILD_TYPE=Release -G Ninja .
-ninja
--- lib/zstd-build.bat
+++
@@ -1,7 +0,0 @@
-cd build\cmake
-mkdir builddir
-cd builddir
-del CMakeCache.txt
-cmake -DCMAKE_BUILD_TYPE=Release -G Ninja ..
-ninja
-cd ..\..\..
